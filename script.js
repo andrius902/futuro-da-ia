b4963f0befbb24d6d29cbfb5672fa9d99e725fc9
andrius902 <andrius.muhlenhoff@escola.pr.gov.br>
2026-09-01 16:56:40 -0300

    aula 2

---
+++ script.js
@@ -0,0 +1,7 @@
+caixaprincipal = document.querySelector(".caixa-principal");
+caixaprincipal = document.querySelector(".caixa-perguntas");
+caixaprincipal = document.querySelector(".caixa-alternativas");
+caixaprincipal = document.querySelector(".caixa-resultado");
+caixaprincipal = document.querySelector(".texto-resultado");
+
+
